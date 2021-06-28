28c7d150377993527b68bedcbbde3bb9afb0c6db
GuzovPavel <pguzov.exceedteam@gmail.com>
2021-06-28 17:08:05 +0300

    fixed tabs

--- src/db/models/task/index.js
+++ src/db/models/task/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 
-const {
-  Schema
-} = mongoose;
+const { Schema } = mongoose;
 
 const taskScheme = new Schema({
   text: String,
--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -1,6 +1,6 @@
 const Task = require("../../db/models/task/index");
 
-module.exports.getAllTasks = (req, res, next) => {
+module.exports.getAllTasks = (req, res) => {
   Task.find().then(result => {
     res.send({
       data: result
@@ -8,18 +8,22 @@ module.exports.getAllTasks = (req, res, next) => {
   });
 };
 
-module.exports.createNewTask = (req, res, next) => {
-const task = new Task(req.body);
+module.exports.createNewTask = (req, res) => {
+  const task = new Task(req.body);
   task.save().then(result => {
     res.send({
       data: result
     });
-  }) 
+  })
 };
 
-module.exports.changeTask = (req, res, next) => {
-const {_id} = req.body;
-  Task.updateOne({_id}, req.body).then(result => {
+module.exports.changeTask = (req, res) => {
+  const {
+    _id
+  } = req.body;
+  Task.updateOne({
+    _id
+  }, req.body).then(result => {
     Task.find().then(result => {
       res.send({
         data: result
@@ -28,7 +32,7 @@ const {_id} = req.body;
   });
 };
 
-module.exports.deleteTask = (req, res, next) => {
+module.exports.deleteTask = (req, res) => {
   Task.deleteOne(req.query).then(result => {
     Task.find().then(result => {
       res.send({
